554f1f8fa267e7bbe52fbb36cbb34449a52d1c98
Kizlo Taras <orleon.nilkford@gmail.com>
2018-11-27 12:46:58 +0200

    Add files via upload

---
+++ Galagram/View/User/PhotoInside.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace View.User
+{
+    /// <summary>
+    /// Interaction logic for PhotoInside.xaml
+    /// </summary>
+    public partial class PhotoInside : System.Windows.Window
+    {
+        /// <summary>
+        /// Initialize a new instance of <see cref="PhotoInside"/>
+        /// </summary>
+        public PhotoInside()
+        {
+            InitializeComponent();
+        }
+        private void MovingWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private void Exit(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
