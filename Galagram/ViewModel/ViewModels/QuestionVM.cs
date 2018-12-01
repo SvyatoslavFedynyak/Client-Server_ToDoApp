22ecc56540158e50f26962091e357525a98d5d59
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-12-01 18:24:11 +0200

    Update QuestionVM.cs

--- Galagram/ViewModel/ViewModels/QuestionVM.cs
+++ Galagram/ViewModel/ViewModels/QuestionVM.cs
@@ -1,18 +1,98 @@
-﻿using System;
+using ViewModel.Commands;
+
 using Models.Entities;
+using View;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace ViewModel.ViewModels
 {
-    public class QuestionVM
+    public class QuestionVM : INotifyPropertyChanged
     {
-        // PROPERTIES
-        Subject[] Subjects { get; set; }
-        string Text { get; set; }
+        // FIELDS
+        private string text;
+        private Subject currentSubject;
 
-        //METHODS
-        public void Send()
+        #region Windows
+        View.User.AskQuestion askQuestionWindow;
+        #endregion
+
+        #region Commands
+        private RelayCommand send;
+        #endregion
+
+        // EVENTS
+        /// <summary>
+        /// Event that invokes when some propery changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // PROPERTIES
+        public ObservableCollection<Subject> Subjects { get; }
+        /// <summary>
+        /// Propetry that enable to interract with current driver
+        /// </summary>
+        /// <returns>Current driver</returns>
+        public Subject CurrentSubject
         {
-            throw new NotImplementedException();
+            get
+            {
+                return currentSubject;
+            }
+            set
+            {
+                currentSubject = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(CurrentSubject)));
+            }
         }
+        public string Text
+        {
+            set
+            {
+                text = value;
+            }
+        }
+
+        #region Commands
+        /// <summary>
+        /// Property that enable to interact with search command.
+        /// </summary>
+        /// <returns>Log In command.</returns>
+        public RelayCommand Send => send;
+        #endregion
+
+        // CONSTRUCTORS
+        /// <summary>
+        /// Basic constructor without parameters.
+        /// </summary>
+        public QuestionVM()
+        {
+            #region Window Initialize
+            askQuestionWindow = null;
+            #endregion
+
+            #region Commands Initialize
+
+            #endregion
+        }
+
+        // METHODS
+        #region Commands
+        private void SendMethod(object obj)
+        {
+
+        }
+        #endregion
+
+        #region Event Raising
+        /// <summary>
+        /// Method that invokes Property Change event
+        /// </summary>
+        /// <param name="e">Property Changed Event Args</param>
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+        #endregion
     }
 }
