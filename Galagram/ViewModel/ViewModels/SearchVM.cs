d5d2303c61812717bccbb38d2b9c64f9ac41f6b9
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-12-01 18:14:09 +0200

    Update SearchVM.cs

--- Galagram/ViewModel/ViewModels/SearchVM.cs
+++ Galagram/ViewModel/ViewModels/SearchVM.cs
@@ -1,19 +1,99 @@
-﻿using System;
+using ViewModel.Commands;
+
 using Models.Entities;
+using View;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace ViewModel.ViewModels
 {
-    class SearchVM
+    class SearchVM : INotifyPropertyChanged
     {
-        // Properties
-        string Data { get; set; }
-        User[] Users { get; set; } 
+        // FIELDS
+        private string data;
+        private User currentUser;
 
-        // Methods
-        public void Search()
+        #region Windows
+        View.User.Search searchWindow;
+        #endregion
+
+        #region Commands
+        private RelayCommand search;
+        #endregion
+
+        // EVENTS
+        /// <summary>
+        /// Event that invokes when some propery changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // PROPERTIES
+        public ObservableCollection<User> Users { get; }
+        /// <summary>
+        /// Propetry that enable to interract with current driver
+        /// </summary>
+        /// <returns>Current driver</returns>
+        public User CurrentUser
         {
-            throw new NotImplementedException();
+            get
+            {
+                return currentUser;
+            }
+            set
+            {
+                currentUser = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(CurrentUser)));
+            }
         }
+        public string Data
+        {
+            set
+            {
+                data = value;
+            }
+        }
+
+        #region Commands
+        /// <summary>
+        /// Property that enable to interact with search command.
+        /// </summary>
+        /// <returns>Log In command.</returns>
+        public RelayCommand Search => search;
+        #endregion
+
+        // CONSTRUCTORS
+        /// <summary>
+        /// Basic constructor without parameters.
+        /// </summary>
+        public SearchVM()
+        {
+            #region Window Initialize
+            searchWindow = null;
+            #endregion
+
+            #region Commands Initialize
+
+            #endregion
+        }
+
+        // METHODS
+        #region Commands
+        private void SearchMethod(object obj)
+        {
+ 
+        }
+        #endregion
+
+        #region Event Raising
+        /// <summary>
+        /// Method that invokes Property Change event
+        /// </summary>
+        /// <param name="e">Property Changed Event Args</param>
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+        #endregion
 
     }
 }
