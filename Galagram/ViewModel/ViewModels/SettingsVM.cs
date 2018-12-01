de44c21e9bc8bafc08793509ffe6daad915f2f9d
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-12-01 18:56:23 +0200

    Update SettingsVM.cs

--- Galagram/ViewModel/ViewModels/SettingsVM.cs
+++ Galagram/ViewModel/ViewModels/SettingsVM.cs
@@ -1,30 +1,107 @@
-﻿using System;
+using ViewModel.Commands;
+
 using Models.Entities;
+using View;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace ViewModel.ViewModels
 {
     public class SettingsVM
     {
+        // FIELDS
+        private string nickname;
+        private string password;
+        private string newPassword;
+
+        #region Windows
+        View.User.Setting settingWindow;
+        #endregion
+
+        #region Commands
+        private RelayCommand apply;
+        private RelayCommand loadNewPhoto;
+        #endregion
+
+        // EVENTS
+        /// <summary>
+        /// Event that invokes when some propery changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
         // PROPERTIES
-        User CurrUser { get; }
-        string NewPass
+        public User currentUser { get; private set; }
+        public string Nickname
         {
             set
             {
-
+                nickname = value;
             }
         }
-        string OldPass { get; }
-        string NickName { get; }
+        public string Password
+        {
+            set
+            {
+                password = value;
+            }
+        }
+        public string NewPassword
+        {
+            set
+            {
+                newPassword = value;
+            }
+        }
+
+        #region Commands
+        /// <summary>
+        /// Property that enable to interact with search command.
+        /// </summary>
+        /// <returns>Log In command.</returns>
+        public RelayCommand Apply => apply;
+        public RelayCommand LoadNewPhoto => loadNewPhoto;
+        #endregion
+
+        // CONSTRUCTORS
+        /// <summary>
+        /// Basic constructor without parameters.
+        /// </summary>
+        public SettingsVM()
+        {
+            #region Window Initialize
+            settingWindow = null;
+            #endregion
+
+            #region Commands Initialize
+
+            #endregion
+        }
 
         // METHODS
-        public void Apply()
+        // METHODS
+        #region Commands
+        private void ApplyMethod(object obj)
         {
-            throw new NotImplementedException();
+
         }
-        public void LoadNewPhoto()
+
+        private void LoadNewPhotoMethod(object obj)
         {
-            throw new NotImplementedException();
+
         }
+
+        #endregion
+
+        #region Event Raising
+        /// <summary>
+        /// Method that invokes Property Change event
+        /// </summary>
+        /// <param name="e">Property Changed Event Args</param>
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+        #endregion
+
     }
 }
