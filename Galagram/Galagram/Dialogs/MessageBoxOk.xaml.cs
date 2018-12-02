ae778203323892a7ffef9b304c9422c29ed4c33b
Kizlo Taras <orleon.nilkford@gmail.com>
2018-12-02 20:53:42 +0200

    Add files via upload

---
+++ Galagram/Galagram/Dialogs/MessageBoxOk.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace Galagram.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for MessageBoxOk.xaml
+    /// </summary>
+    public partial class MessageBoxOk : System.Windows.Window
+    {
+        // CONSTRUCTORS
+        public MessageBoxOk()
+        {
+            InitializeComponent();
+        }
+        // PROPERTIES
+        public string Header
+        {
+            get
+            {
+                return HeaderLbl.Content.ToString();
+            }
+            set
+            {
+                HeaderLbl.Content = value;
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return ContentTb.Text;
+            }
+            set
+            {
+                ContentTb.Text = value;
+            }
+        }
+        // METHODS
+        private void MovingWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private void Exit(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Galagram/Galagram/Dialogs/MessageBoxYesNo.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace Galagram.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for MessageBoxYesNo.xaml
+    /// </summary>
+    public partial class MessageBoxYesNo : System.Windows.Window
+    {
+        // CONSTRUCTORS
+        public MessageBoxYesNo()
+        {
+            InitializeComponent();
+        }
+        // PROPERTIES
+        public string Header
+        {
+            get
+            {
+                return HeaderLbl.Content.ToString();
+            }
+            set
+            {
+                HeaderLbl.Content = value;
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return ContentTb.Text;
+            }
+            set
+            {
+                ContentTb.Text = value;
+            }
+        }
+        // METHODS
+        private void MovingWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private void Yes(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+        private void No(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
