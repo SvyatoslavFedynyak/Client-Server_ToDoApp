8f8245f1d3ad4a8a19ef9183266e85482d740603
Kizlo Taras <orleon.nilkford@gmail.com>
2018-12-02 20:58:20 +0200

    Feature/binding squash (#27)

---
+++ Galagram/Galagram/User/Follow.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Galagram.User
+{
+    /// <summary>
+    /// Interaction logic for Follow.xaml
+    /// </summary>
+    public partial class Follow : System.Windows.Window
+    {
+        public Follow()
+        {
+            InitializeComponent();
+        }
+        public string Header
+        {
+            get
+            {
+                return HeaderLbl.Content.ToString();
+            }
+            set
+            {
+                HeaderLbl.Content = value;
+            }
+        }
+        private void MovingWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private void Exit(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
