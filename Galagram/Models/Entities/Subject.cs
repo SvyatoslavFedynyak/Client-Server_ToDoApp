2d77c259b7459d20eebd1dc95d9c23f3eb301fd5
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-12-02 13:34:04 +0200

    Update Subject.cs

--- Galagram/Models/Entities/Subject.cs
+++ Galagram/Models/Entities/Subject.cs
@@ -1,9 +1,52 @@
-﻿namespace Models.Entities
+namespace Models.Entities
 {
+    /// <summary>
+    /// Represents class that models Subject
+    /// </summary>
     public class Subject
     {
-        // Properties
-        public int Id { get; set; }
-        public string Name { get; set; }
+        // FIELDS
+        private int id;
+        private string name;
+        // PROPERTIES
+        /// <summary>
+        /// Property that defines the id
+        /// </summary>
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        /// <summary>
+        /// Property that defines the name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters
+        /// </summary>
+        /// <param name="id">Id subject</param>
+        /// <param name="name">Name subject</param>
+        public Subject(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
     }
 }
