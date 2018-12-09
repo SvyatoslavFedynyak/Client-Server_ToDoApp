553f0eeee7e4e542fc2d1d81ab869019b0fc63f1
Svyatoslav Fedynyak <svyatoslav912@gmail.com>
2018-12-09 20:34:20 +0200

    Config update

---
+++ Galagram/ViewModel/Configuration/Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace ViewModel.Configuration
+{
+    class Configuration
+    {
+        public const string APP_NAME = "Galagram";
+        public const string PHOTOS_SAVE_PATH = @"..\..\Data\Photos\";
+        public const string CANT_CONNECT_TO_DB_MESSAGE = "Data Base connection error";
+        public const string NO_SUCH_FILE_MESSAGE = "Current file is missed";
+        public const string NO_SUCH_PROFILE_MESSAGE = "There is no such profile";
+    }
+}
